265f5d0dc4b0489ee5bc8de945c1c6abbda35000
thientofu <toffu05062002@gmail.com>
2024-10-19 10:25:17 +0700

    2nd

---
+++ src/baitap/BaiTap/bai3.java
@@ -0,0 +1,10 @@
+package baitap.BaiTap;
+
+public class bai3 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+        int c = Math.max(a,b);
+        System.out.println("gia tri c:"+ c);
+    }
+}
