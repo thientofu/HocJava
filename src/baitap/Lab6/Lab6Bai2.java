e19e47d989dc27207bda3e74f46ac46d075051e1
thientofu <toffu05062002@gmail.com>
2024-11-03 09:09:55 +0700

    commitLab6Bai2

--- src/baitap/Lab6/Lab6Bai2.java
+++ src/baitap/Lab6/Lab6Bai2.java
@@ -1,4 +1,13 @@
 package baitap.Lab6;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Lab6Bai2 {
+    public static void main(String[] args) {
+        Pattern CCCD = Pattern.compile("[0-9]{10}");
+        Matcher m = CCCD.matcher("156324896a");
+        System.out.println("CCCD = " + m.matches());
+    }
+
 }
