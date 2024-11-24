ae3d370a8022fc1d6cdefe9ec1d80590fa1c59fd
Admin <toffu05062002@gmail.com>
2024-11-24 08:43:23 +0700

    24 /11/2024

--- src/baitap/Lab11Hoc/HelloWorld.java
+++ src/baitap/Lab11Hoc/HelloWorld.java
@@ -1,6 +1,8 @@
 package baitap.Lab11Hoc;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class HelloWorld extends Thread {
     @Override
@@ -14,10 +16,30 @@ public class HelloWorld extends Thread {
 // TODO Auto-generated method stub
     }
     public static void main(String[] args) throws IOException {
+        Date date = new Date();
+        System.out.println("start [" + date.toString() + "] " );
+
         int MAX = 2;
         for (int i = 0; i < MAX; i++) {
             (new HelloWorld()).start();
         }
-        System.out.println("finish");
+
+//        long sum1 = 0L;
+//        long sum2 = 0L;
+//
+//        for (long i = 0L; i < 10000000000L; i++) {
+//            sum1 += i;
+//        }
+//
+//        for (long i = 0L; i < 10000000000L; i++) {
+//            sum2 += i;
+//        }
+        Date date_end = new Date();
+        System.out.println("finish [" + date_end.toString() + "] " );
+        long diffInMillies = Math.abs(date.getTime() - date_end.getTime());
+        long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+        System.out.println("excution time :" + diff + " SECONDS" );
+
+
     }
 }
