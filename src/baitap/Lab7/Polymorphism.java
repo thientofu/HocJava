5453c4b268ca6841b3f7546a0c7ee5524d1cb932
Admin <toffu05062002@gmail.com>
2024-11-30 19:46:19 +0700

    24 /11/2024

--- src/baitap/Lab7/Polymorphism.java
+++ src/baitap/Lab7/Polymorphism.java
@@ -10,10 +10,13 @@ class Dog extends Animal{
     void eat(){System.out.println("eating fruits");}
 }
 class BabyDog extends Dog{
+    int age;
+    int weitght;
     void eat(){System.out.println("drinking milk");}
     public static void main(String args[]){
-        Animal a1;
+        Animal a1,a2;
         a1 = new BabyDog();
+        a2 = new BabyDog();
 
 
         a1.eat();
